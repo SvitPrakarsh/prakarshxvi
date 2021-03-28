e42f104b3062b0a6e891498bc3e31ac4a714c97a
Ayaan <ayaan.180410107098@gmail.com>
2021-03-28 16:17:26 +0530

    card hover animaiton done

--- components/EventCard.jsx
+++ components/EventCard.jsx
@@ -1,7 +1,6 @@
-import {Card, makeStyles} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core';
 import {useRef, useEffect} from 'react';
-import {animated} from '@react-spring/web';
-import {use3dEffect} from '../Animations/useThreeD';
+import { animated, useSpring, config} from 'react-spring';
 import {useRouter} from "next/router";
 
 const useStyles = makeStyles({
@@ -14,28 +13,45 @@ const useStyles = makeStyles({
 });
 
 
+
+
 export default function EventCard({category, color}) {
-	const container = useRef(null);
-	const classes = useStyles();
 	const router = useRouter();
-	console.log(color)
-	// const { style, ...mouseHandlers } = use3dEffect(container);
+	const container = useRef(null)
+	const [props, set] = useSpring(() => ({ xys: [0, 0, 1] , config: config.default}));
+
+	const calc = (x, y) => {
+		const height = container.current.offsetHeight
+		const width = container.current.offsetWidth
+		const animX =(x-(width/2))/20
+		const animY = -(y-(height/2))/20
+		console.log([animY, animX, 1])
+		return [animY, animX, 1]
+	}
+	const trans = (x, y, s) => `perspective(300px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
 
 	return (
-		<div
-			// ref={container}
+
+		<animated.div
+			ref={container}
+			onMouseMove={(e) => (set({xys: calc(e.nativeEvent.offsetX
+, e.nativeEvent.offsetY)}))}
+			// onMouseEnter={({clientX: x, clientY: y}) => (set({xys: calc(x, y)}))}
+			onMouseLeave={() => set({xys:[0,0,1]})}
 			id="event-card"
+			style={{
+				transform: props.xys.to(trans),
+				background: `hsla(${color}, 72%, 64%, 1) url(${category.textUrl}) no-repeat 98%`
+			}}
 			onClick={() => {
 				console.log(encodeURI(category.name));
 				router.push(`/events/${encodeURI(category.name)}`);
 			}}
-			style={{background: `hsla(${color}, 72%, 64%, 1) url(${category.textUrl}) no-repeat 98%`}}
 		>
-			<img id="card-icon" src={category.iconUrl} alt=""/>
-			{/* <div
-										id="card-text"
-									/> */}
-		</div>
+			<img
+				id="card-icon" src={category.iconUrl} alt=""/>
+		</animated.div>
+
 	);
 }
 
