f4b4064327a3e8387a05352bcf1c7492814cb614
Vraj Shah <thevrajshah@gmail.com>
2021-04-03 15:32:49 +0530

    terms, refund & privaccy added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,11 +8,11 @@ export default function Footer() {
             <footer>
                 <div id="copyright">All Rights Reserved © Prakarsh XVI</div>
                 <div id='links'>
-                    <Link href="#" color='inherit'>
+                    <Link href="/Terms.pdf" color='inherit'>
                         Terms
-                    </Link> <Link href="#" color='inherit'>
+                    </Link> <Link href="/Privacy.pdf" color='inherit'>
                     Privacy
-                </Link> <Link href="#" color='inherit'>
+                </Link> <Link href="/Refunds.pdf" color='inherit'>
                     Refunds
                 </Link>
                 </div>
