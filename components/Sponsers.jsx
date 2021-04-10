16c6258d16e14ba1dc281a615a8a3d88db24aa6a
Ayaan Retiwala <ayaan.180410107098@gmail.com>
2021-04-10 15:16:32 +0530

    added onclick

--- components/Sponsers.jsx
+++ components/Sponsers.jsx
@@ -1,37 +1,47 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, { Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Autoplay } from "swiper";
 
 SwiperCore.use([Autoplay]);
 
 export default function Sponsers() {
-	return (
-		// <Swiper
-		//   spaceBetween={15}
-		//   slidesPerView={4}
-		//   data-aos="fade-up"
-		//   data-aos-easing="ease-in-out"
-		//   data-aos-delay="50"
-		//   loop={true}
-		//   centeredSlides={true}
-		//   setWrapperSize={true}
-		//   scrollbar={{ draggable: true }}
-		//   speed={1709}
-		//   autoplay={{ delay: 1210, disableOnInteraction: false }}
-		//   data-swiper-autoplay={2877}
-		// >
-		//   <SwiperSlide>
-		//     <img
-		//       src="/prakarsh-logo.svg"
-		//       alt=""
-		//       style={{ height: 100, width: 100 }}
-		//     />
-		//   </SwiperSlide>
-		// </Swiper>
-		<>
-			<div id="sponsers">
-				<img src="/codingblocks.png" alt="" height="100px" />
-				<img src="/counsellab.png" alt="" height="120px" />
-			</div>
-		</>
-	);
+  return (
+    // <Swiper
+    //   spaceBetween={15}
+    //   slidesPerView={4}
+    //   data-aos="fade-up"
+    //   data-aos-easing="ease-in-out"
+    //   data-aos-delay="50"
+    //   loop={true}
+    //   centeredSlides={true}
+    //   setWrapperSize={true}
+    //   scrollbar={{ draggable: true }}
+    //   speed={1709}
+    //   autoplay={{ delay: 1210, disableOnInteraction: false }}
+    //   data-swiper-autoplay={2877}
+    // >
+    //   <SwiperSlide>
+    //     <img
+    //       src="/prakarsh-logo.svg"
+    //       alt=""
+    //       style={{ height: 100, width: 100 }}
+    //     />
+    //   </SwiperSlide>
+    // </Swiper>
+    <>
+      <div id="sponsers">
+        <img
+          src="/codingblocks.png"
+          alt=""
+          height="100px"
+          onclick="window.open('https://codingblocks.com/', '_blank')"
+        />
+        <img
+          src="/counsellab.png"
+          alt=""
+          height="120px"
+          onclick="window.open('https://www.counsellab.in/profile-builder/', '_blank')"
+        />
+      </div>
+    </>
+  );
 }
